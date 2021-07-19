d8a3efe9d4af4278ef948c38c535b11c80c54242
bsuraj23 <bsuraj23@gmail.com>
2021-07-19 21:33:02 +0530

    nothing much as code justa sample handson to understand git push

---
+++ package2/ExceptionPackage/PujithAnu.java
@@ -0,0 +1,9 @@
+package package2.ExceptionPackage;
+
+public class PujithAnu {
+
+    public static void main(String args[]){
+        System.out.println("this is example for git push which was added from local with pujith and anu ");
+
+    }
+}
